4f24dd6a24d3a5c7f104c6d89debe11964d214bd
PkHutch <pk-hutch@hotmail.com>
2025-02-25 13:55:29 -0700

    time-class: Add builtin overloads for time-class
    Add the operators for equality comparisons and subtraction. It is a bit
    odd how the subtraction is giving back the minutes. I think ideally it
    would be something more like giving another time object, but the
    behaviour can stay for now.

--- room_time.py
+++ room_time.py
@@ -1,16 +1,15 @@
 """ The room_time module contains the RoomTime class for handling the available times of a room. """
 from datetime import time
 
+# TODO: Clean up docstring testing / overall documentation.
 class RoomTime:
     """
     Represents a room time based on a string format (HHMM or HMM).
 
-    >>> rt = RoomTime("930")
-    >>> rt.room_time
+    >>> RoomTime("930").room_time
     datetime.time(9, 30)
 
-    >>> rt = RoomTime("1230")
-    >>> rt.room_time
+    >>> RoomTime("1230").room_time
     datetime.time(12, 30)
 
     >>> rt = RoomTime("2560")
@@ -22,13 +21,88 @@ class RoomTime:
     Traceback (most recent call last):
     ...
     ValueError: Room time must be a 3 or 4-digit string representing HHMM, given time_string: '22'
+
+    >>> RoomTime("1230") == RoomTime("1230")
+    True
+
+    >>> RoomTime("1230") == RoomTime("1240")
+    False
+
+    >>> RoomTime("1230") != RoomTime("1230")
+    False
+
+    >>> RoomTime("1230") != RoomTime("1240")
+    True
+
+    >>> RoomTime("1200") < RoomTime("1240")
+    True
+
+    >>> RoomTime("1200") < RoomTime("1200")
+    False
+
+    >>> RoomTime("1230") < RoomTime("1200")
+    False
+
+    >>> RoomTime("1230") > RoomTime("1200")
+    True
+
+    >>> RoomTime("1200") > RoomTime("1200")
+    False
+
+    >>> RoomTime("1230") > RoomTime("1400")
+    False
+
+    >>> RoomTime("1230") >= RoomTime("1200")
+    True
+
+    >>> RoomTime("1200") >= RoomTime("1200")
+    True
+
+    >>> RoomTime("1230") >= RoomTime("1300")
+    False
+
+    >>> RoomTime("1100") <= RoomTime("1200")
+    True
+
+    >>> RoomTime("1200") <= RoomTime("1200")
+    True
+
+    >>> RoomTime("1230") <= RoomTime("1200")
+    False
+
+    >>> RoomTime("1200") - RoomTime("1200")
+    0
+
+    >>> RoomTime("0000") - RoomTime("1200")
+    -720
+
+    >>> RoomTime("1200") - RoomTime("0000")
+    720
     """
     def __init__(self, time_string: str) -> None:
         self.room_time: time = time_string
 
+    def __eq__(self, other: "RoomTime") -> bool:
+        return self._room_time == other.room_time
+
+    def __neq__(self, other: "RoomTime") -> bool:
+        return self._room_time != other.room_time
+
+    def __lt__(self, other: "RoomTime") -> bool:
+        return self._room_time < other.room_time
+
     def __gt__(self, other: "RoomTime") -> bool:
-        # TODO: Implement this and remaining comparisons.
-        return True
+        return self._room_time > other.room_time
+
+    def __le__(self, other: "RoomTime") -> bool:
+        return self._room_time <= other.room_time
+
+    def __ge__(self, other: "RoomTime") -> bool:
+        return self._room_time >= other.room_time
+
+    def __sub__(self, other: "RoomTime") -> int:
+        return (60 * (self._room_time.hour - other._room_time.hour)
+                 + self._room_time.minute - other.room_time.minute)
 
     @property
     def room_time(self) -> time:
