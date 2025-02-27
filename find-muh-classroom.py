44e7a4c73da85e2e246b5cab879629d080ef6f5b
PkHutch <pk-hutch@hotmail.com>
2025-02-27 13:01:25 -0700

    time-class: Refactoring code into classes for easier testing and managing structure (#1)
    * Add RoomTime class
    * Refactor find-muh-classroom
    * Add builtin overloads for time-class
    * Update documentation and testing

--- find-muh-classroom.py
+++ find-muh-classroom.py
@@ -1,15 +1,16 @@
 import code
 import json
+from room_time import RoomTime
 # import type
 
 
 def main():
-
     #find_the_openest("rooms.json", "monday", 1701)
-    find_the_longestest("rooms.json", "tuesday", 951, "B")
-
+    # TODO: Add time.now() call or similar.
+    find_the_longestest("rooms.json", "tuesday", 1500, "EB")
 
 def find_the_longestest(input_file, day, time, wing):
+    # TODO: Encoding specification.
     f = open(input_file, "rt")
     wings = json.loads(f.read())
     f.close()
@@ -24,26 +25,31 @@ def find_the_longestest(input_file, day, time, wing):
     for room in open_rooms:
         print(f"{room[0]} is booked in {room[1]} minutes")
 
-
-
+# TODO: Wing is not used in this function, might be worth removing.
 def time_till_booked(day, time, wing, room):
     current_shortest_time = 0
     try:
         for time_slot in room[day]:
-            if (convert_time(time) > convert_time(time_slot[0]) and (convert_time(time) < convert_time(time_slot[1]))):
-                #ruh roh, we're booked
-                return -1
-            if (convert_time(time_slot[0]) - convert_time(time)) < current_shortest_time:
-                current_shortest_time = convert_time(time_slot[0]) - convert_time(time)
+            try:
+                # TODO: Clarify if this is intended logic, made some assumptions.
+                CURRENT_TIME: RoomTime = RoomTime(str(time))
+                BOOKED_START: RoomTime = RoomTime(str(time_slot[0]))
+                BOOKED_END: RoomTime = RoomTime(str(time_slot[1]))
+
+                if BOOKED_START <= CURRENT_TIME <= BOOKED_END:
+                    return -1
+
+                current_shortest_time = min(BOOKED_START - CURRENT_TIME, current_shortest_time)
+
+            except ValueError as e:
+                print(e)
+
         return current_shortest_time
     except KeyError:
         return -1
 
-    
-
-
-
 def find_the_openest(input_file, day, time): # basically, whats gonna be open till tomorrwo
+    # TODO: Encoding specification.
     f = open(input_file, "rt")
     wings = json.loads(f.read())
     f.close()
@@ -53,31 +59,20 @@ def find_the_openest(input_file, day, time): # basically, whats gonna be open ti
     print(f"time: {time} or {time-1200}pm?")
     for wing in wings.keys():
         print(f"wing: {wing}") # wing wing, hello? moshi moshi???
-        
+
         for room in wings[wing].keys():
             bad_room = False
-            if not (day in wings[wing][room]):
+            if not day in wings[wing][room]:
                 # print(f"    oh shit! {room} is just always open on {day}")
                 continue
-            for i, timeSlot in enumerate(wings[wing][room][day]):
-                if not (wings[wing][room][day][i][1] < time):
+            for i in enumerate(wings[wing][room][day]):
+                if not wings[wing][room][day][i][1] < time:
                     bad_room = True # idk how else to do this
                     break
         if not bad_room:
             print(f"    {room} is open for the rest of the day")
-                
+            # TODO: Determine if this is intended to be nested in the for loop.
 
-def convert_time(raw_time):
-    if len(str(raw_time)) == 3:
-        return 60 * int(str(raw_time)[0]) + int(str(raw_time)[1:3])
-    elif len(str(raw_time)) == 4:
-        return 60 * int(str(raw_time)[0:2]) + int(str(raw_time)[2:4])
-    else:
-        print("bro what what other times do we have")
-        return 0
-
-
-        
 main()
 
 code.interact(local = locals())
